913889e5282ac6e0c2d8dde75a7f3fe5c9b1b7a9
hov8aa <hov8aa@gmail.com>
2024-09-21 11:48:20 +0530

    updated the code to take input

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../add'
 
 RSpec.describe 'add' do
-    it "adding 2 and 2 gives 4" do
+    it "adding a and b gives a+b" do
         expect(add(2,2)).to eq(4)
+        expect(add(5, -3)).to eq(2)
+        expect(add(-5, -3)).to eq(-8)
+        expect(add(0, 7)).to eq(7)
     end
 end    
